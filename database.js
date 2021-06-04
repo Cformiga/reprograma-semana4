035eb54e7687cbc434de4a9c5af0edbd5f0adf19
Cformiga <cformiga@gmail.com>
2021-06-04 13:24:40 -0300

    adicionadodatabase

---
+++ database.js
@@ -0,0 +1,43 @@
+const livros = [
+    {
+        nome: "Jardim das Borboletas",
+        autor: "valter dos Santos",
+        categoria: "Espirita",
+        paginas: 346,
+        recomenda: "true",
+        leu: true,
+
+    },
+
+    {
+        nome:" O Alquimista",
+        autor: "Paulo Coelho",
+        categoria: "Auto Conhecimento",
+        paginas: 208,
+        recomenda: "true",
+        leu: true,
+
+    },
+
+    {
+        nome: "A Cabana",
+        autor: "Willian P Young",
+        categoria: "Auto Conhecimento",
+        paginas: 192,
+        recomenda: "false",
+        leu: true,
+
+    },
+
+    {
+        nome: "O Poder do Hábito",
+        autor: " Charles Duhhig",
+        categoria: "Organizacao",
+        paginas: 408,
+        recomenda: "true",
+        leu: true,
+
+    },
+  
+]
+module.exports = livros
